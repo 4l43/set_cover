a4d47fb0707be38eb3b07d1893ba72daa2d6fd19
4l43 <103286987+4l43@users.noreply.github.com>
2024-10-12 19:10:26 +0200

    Update set_cover.c V2

--- set_cover.c
+++ set_cover.c
@@ -1,81 +1,137 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int U = 7;  // Taille de l'ensemble U
-int S = 6;  // Nombre de sous-ensembles
-char c[] = "1001001 1001000 0001101 0010110 0110011 0100001";
-int tab[6][7];  // Tableau pour les sous-ensembles
+int U = 7;
+int S = 6;
+int A[6][7] = {
+    {1, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 1},
+    {0, 0, 1, 0, 1, 1, 0},
+    {0, 1, 1, 0, 0, 1, 1},
+    {0, 1, 0, 0, 0, 0, 1}
+};
 
-bool solution_found = false;  // Variable pour indiquer si une solution a été trouvée
+bool elements_couverts[7];
 
-// Fonction pour vérifier si une combinaison de sous-ensembles couvre tout U
-bool is_combination_covered(int subsets[], int size) {
-    bool covered[U];
+void initialiser_elements_couverts() {
     for (int i = 0; i < U; i++) {
-        covered[i] = false;  // Initialiser tout comme non couvert
+        elements_couverts[i] = false;
     }
+}
 
-    // Marquer les éléments couverts par les sous-ensembles de la combinaison
-    for (int i = 0; i < size; i++) {
-        int subset_index = subsets[i];
-        for (int j = 0; j < U; j++) {
-            if (tab[subset_index][j] == 1) {
-                covered[j] = true;
+int compter_elements_non_couverts(int sous_ensemble[]) {
+    int compte = 0;
+    for (int i = 0; i < U; i++) {
+        if (sous_ensemble[i] == 1 && !elements_couverts[i]) {
+            compte++;
+        }
+    }
+    return compte;
+}
+
+void marquer_comme_couvert(int sous_ensemble[]) {
+    for (int i = 0; i < U; i++) {
+        if (sous_ensemble[i] == 1) {
+            elements_couverts[i] = true;
+        }
+    }
+}
+
+void affichage_sous_ensembles(int sous_ensembles[], int taille) {
+    for (int i = 0; i < taille; i++) {
+        printf("Sous-ensemble %c\n", 'A' + sous_ensembles[i]);
+    }
+}
+
+void algorithme_glouton() {
+    initialiser_elements_couverts();
+    
+    bool tous_couverts = false;
+    while (!tous_couverts) {
+        int meilleur_index = -1;
+        int meilleur_compte = -1;
+
+        for (int i = 0; i < S; i++) {
+            int compte = compter_elements_non_couverts(A[i]);
+            if (compte > meilleur_compte) {
+                meilleur_compte = compte;
+                meilleur_index = i;
+            }
+        }
+
+        if (meilleur_compte == 0) {
+            break;
+        }
+
+        marquer_comme_couvert(A[meilleur_index]);
+        printf("Selection du sous-ensemble %c\n", 'A' + meilleur_index);
+
+        tous_couverts = true;
+        for (int i = 0; i < U; i++) {
+            if (!elements_couverts[i]) {
+                tous_couverts = false;
+                break;
             }
         }
     }
 
-    // Vérifier si tous les éléments de U sont couverts
+    if (tous_couverts) {
+        printf("Tous les elements sont couverts.\n");
+    } else {
+        printf("Impossible de couvrir tous les elements avec les sous-ensembles donnes.\n");
+    }
+}
+
+bool est_combinaison_couverte(int sous_ensembles[], int taille) {
+    bool couverts[U];
     for (int i = 0; i < U; i++) {
-        if (!covered[i]) {
+        couverts[i] = false;
+    }
+
+    for (int i = 0; i < taille; i++) {
+        int index_sous_ensemble = sous_ensembles[i];
+        for (int j = 0; j < U; j++) {
+            if (A[index_sous_ensemble][j] == 1) {
+                couverts[j] = true;
+            }
+        }
+    }
+
+    for (int i = 0; i < U; i++) {
+        if (!couverts[i]) {
             return false;
         }
     }
     return true;
 }
 
-// Fonction pour générer toutes les combinaisons de taille 'r' de sous-ensembles
-void find_optimal_cover_recursive(int subsets[], int start, int size, int r) {
-    if (solution_found) return;  // Si une solution a été trouvée, on arrête
-
-    if (size == r) {
-        // Si la combinaison a la bonne taille, vérifier si elle couvre tout U
-        if (is_combination_covered(subsets, r)) {
-            printf("Combinaison optimale trouvée avec %d sous-ensembles :\n", r);
-            for (int i = 0; i < r; i++) {
-                printf("Sous-ensemble %c\n", 'A' + subsets[i]);  // Convertir en lettre
-            }
-            solution_found = true;  // On indique qu'une solution a été trouvée
+void combinaisons_exactes(int sous_ensembles[], int debut, int taille, int r) {
+    if (taille == r) {
+        if (est_combinaison_couverte(sous_ensembles, r)) {
+            printf("Combinaison optimale avec %d sous-ensembles :\n", r);
+            affichage_sous_ensembles(sous_ensembles, r);
         }
         return;
     }
 
-    // Générer toutes les combinaisons possibles
-    for (int i = start; i < S; i++) {
-        subsets[size] = i;
-        find_optimal_cover_recursive(subsets, i + 1, size + 1, r);
+    for (int i = debut; i < S; i++) {
+        sous_ensembles[taille] = i;
+        combinaisons_exactes(sous_ensembles, i + 1, taille + 1, r);
+    }
+}
+
+void algorithme_exact() {
+    for (int r = 1; r <= S; r++) {
+        int sous_ensembles[r];
+        combinaisons_exactes(sous_ensembles, 0, 0, r);
     }
 }
 
 int main() {
-    // Initialisation du tableau tab à partir de la chaîne c
-    int row = 0, col = 0;
-    for (int i = 0; i < 42; i++) {
-        if (c[i] == ' ') {
-            row++;
-            col = 0;
-        } else {
-            tab[row][col] = c[i] - '0';
-            col++;
-        }
-    }
-
-    // Test de toutes les combinaisons de 2 à S sous-ensembles
-    for (int r = 2; r <= S; r++) {
-        if (solution_found) break;  // Arrêter la boucle si une solution a été trouvée
-        int subsets[r];
-        find_optimal_cover_recursive(subsets, 0, 0, r);
-    }
-
+    printf("Algorithme Glouton:\n");
+    algorithme_glouton();
+    printf("\nAlgorithme Exact:\n");
+    algorithme_exact();
     return 0;
 }
